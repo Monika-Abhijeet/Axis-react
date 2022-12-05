e54f04febd73f73a1abc19e9951600b0047c52cf
Monika Abhijeet <monikaabhijeet@Monikas-MacBook-Pro.local>
2022-12-05 12:45:53 +0530

    initial react setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Header from "./components/Header/Header";
+
 function App() {
   return (
     <div className="App">
       <h1>Hello Monika</h1>
+      <h1>Hi everyone how are you hello?</h1>
+      <Header></Header>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import "./Header.css";
+function Header() {
+  return (
+    <div>
+      <h1 className="header-wrapper">Header</h1>
+    </div>
+  );
+}
+
+export default Header;
